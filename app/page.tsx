3cdc393a20d7ef797adef574a7a64219342143ca
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-08-14 21:56:03 +0600

    render buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
-import { PrimaryButton } from '@/components/Button';
+import { LinkButton, PrimaryButton } from '@/components/Button';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+
 
 const Home = () => {
   const router = useRouter();
@@ -13,11 +14,15 @@ const Home = () => {
         <div className='font-inter font-semibold text-base p-5'>
           <h3 style={{color: "#E9D7FE"}}>Fast. Simple. Banking</h3>
         </div>
-        <div className="font-inter tracking-wide font-semibold text-7xl text-gray-50 w-96" style={{width: "790px"}}>
+        <div className="font-inter tracking-wide font-medium text-7xl text-gray-50 w-96" style={{width: "790px"}}>
           <h1>Keep spending with the best cards.</h1>
         </div>
         <div className='text-center font-inter text-lg font-normal text-gray-800 leading-[150%] p-10'>
           <h3 style={{color: "#E9D7FE"}}>Best banking cards made simplified.</h3>
+        </div>
+        <div className='center space-x-6'>
+            <button className='rounded-[8px] w-36 h-14 text-lg' style={{backgroundColor: "#FFF"}}>Login</button>
+            <button className='rounded-[8px] w-36 h-14 text-lg' style={{color: "#FFF", backgroundColor: "#7F56D9"}}>Register</button>
         </div>
       </div>
     </main>
