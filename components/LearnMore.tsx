66b52989701dbb8dd6f99328e7f733605827c44a
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-10-06 19:39:12 +0600

    complete learn more section

--- components/LearnMore.tsx
+++ components/LearnMore.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import Image from 'next/image';
+import { Button } from 'antd';
+import PlaySvg from './ui/PlaySvg';
 
 const LearnMore = (): JSX.Element => {
   return (
-    <section className='h-[640px]'>
-        <div className='bg-[#FFEEDD] h-[600px] skew-y-3 my-10'>
-            <div className='mx-40'>
-                <div className='transform -skew-y-3 pt-40 w-7/12'>
-                    <h1 className='text-[42px] font-semibold leading-10 tracking-tight'>
+    <section className='h-[720px]'>
+        <div className='bg-[#FFEEDD] h-[680px] skew-y-3 my-10'>
+            <div className='mx-40 flex'>
+                <div className='transform -skew-y-3 pt-40'>
+                    <h1 className='text-[42px] font-semibold leading-tight tracking-tight'>
                         Get even more financial clarity with Nest Finance.
                     </h1>
                     <h2 className='text-[22px] mt-6'>
@@ -16,6 +18,22 @@ const LearnMore = (): JSX.Element => {
                     With everything connected, you'll get timely nerdy insights so you can be sure you're making
                      the right financial decisions for you. Best of all, it's free.
                     </h2>
+                    <div className='space-x-8 mt-10 flex'>
+                        <Button className='py-6 px-12 h-20 bg-gradient-to-b from-[#7F56D9] to-[#53389e] text-white text-[18px] font-semibold'>Learn More</Button>
+                        <Button className='py-6 px-12 h-20  text-[#F44E4D] text-[18px] border-2 border-[#F44E4D] font-semibold flex space-x-3'>
+                            <PlaySvg/>
+                            Watch Now
+                        </Button>
+                    </div>
+                </div>
+                <div className='mt-36'>
+                    <Image
+                      src="/brand_assets/manIllustr.png"
+                      width={1400}
+                      height={40}
+                      alt="Picture of the author"
+                      className=''
+                    />
                 </div>
             </div>
         </div>
---
+++ components/ui/PlaySvg.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Button } from 'antd';
+
+const PlaySvg = (): JSX.Element => {
+
+  return (
+    <svg className='my-auto' xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+      <g filter="url(#filter0_b_98_3008)">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M12.7387 24C19.3661 24 24.7387 18.6274 24.7387 12C24.7387 5.37258 19.3661 0 12.7387 0C6.11129 0 0.738708 5.37258 0.738708 12C0.738708 18.6274 6.11129 24 12.7387 24ZM10.8637 16.4016L17.6137 12.6288C18.1137 12.3493 18.1137 11.6507 17.6137 11.3712L10.8637 7.59836C10.3637 7.31889 9.73871 7.66823 9.73871 8.22717V15.7728C9.73871 16.3318 10.3637 16.6811 10.8637 16.4016Z" fill="#F44E4D"/>
+      </g>
+      <defs>
+        <filter id="filter0_b_98_3008" x="-15.2613" y="-16" width="56" height="56" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feGaussianBlur in="BackgroundImageFix" stdDeviation="8"/>
+          <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_98_3008"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_98_3008" result="shape"/>
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default PlaySvg;
