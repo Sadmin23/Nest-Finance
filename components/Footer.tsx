3c19132e03c2d6f4f76d748010b1c54adc42dcdf
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-10-07 20:09:47 +0600

    Complete Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,32 +3,34 @@ import { useRouter } from 'next/navigation';
 import Image from 'next/image'
 import Link from 'next/link';
 import NFLogoLarge from './SVG/NFLogoLarge';
+import FooterIllustration from './Illustration/FooterIllustration';
 
 const Footer = (): JSX.Element => {
   const router = useRouter();
   return (
     <div className="flex-col bg-gradient-to-b from-[#53389e] to-[#684CB4]" >
-        <div className='flex border-b-2 mx-40 pt-16 pb-20 border-white'>
+        <div className='flex border-b-2 mx-40 pt-11 border-white'>
             <div className='flex-col'>
                 <Link href="/">
                   <NFLogoLarge/>
                 </Link>
-                <h2 className='text-white mt-10 w-96'>This PRD outlines the features and functionality of a comprehensive banking and financial information platform.</h2>
+                <h2 className='text-white mt-10 w-96 leading-7'>This PRD outlines the features and functionality of a comprehensive banking and financial information platform.</h2>
             </div>
-            <div className='flex flex-col text-white space-y-6 ml-28'>
+            <div className='flex flex-col text-white space-y-6 ml-28 mt-11'>
               <Link href="/">Home</Link>
               <Link href="/all-banks">Banking</Link>
               <Link href="/card-details">Credit Cards</Link>
-              <Link href="/atm">ATMs</Link>
+              <Link href="/atm">Mutual Funds</Link>
             </div>
-            <div className='flex flex-col text-white space-y-6 ml-28'>
+            <div className='flex flex-col text-white space-y-6 ml-28 mt-11 mb-10'>
               <Link href="/">Deposits</Link>
               <Link href="/all-banks">Loans</Link>
               <Link href="/card-details">Bonds</Link>
               <Link href="/atm">Deposit Scheme</Link>
             </div>
+            <FooterIllustration/>
         </div>
-        <div className='flex mx-40 py-10 font-inter font-normal text-base text-[#D6BBFB]'>
+        <div className='flex mx-40 mt-6 pb-20 font-inter font-normal text-base text-[#D6BBFB]'>
             <h2>@2023. All rights reserved.</h2>
             <div className='ml-auto mr-0 space-x-6'>
                 <Link href="/">Terms & Services</Link>
--- components/PopularBanks.tsx
+++ components/PopularBanks.tsx
@@ -7,8 +7,8 @@ const PopularBanks = (): JSX.Element => {
 
   return (
     <section className='mx-40'>
-        <h1 className='text-[42px] font-semibold leading-10 tracking-tight mt-20'>Popular Banks List</h1>
-        <div className='flex-col space-y-10 mt-32 mb-16'>
+        <h1 className='text-[42px] font-semibold leading-10 tracking-tight mt-14'>Popular Banks List</h1>
+        <div className='flex-col space-y-10 mt-28 mb-16'>
           <div className='flex space-x-14'>
             <PopularBanksCard x={1}/>
             <PopularBanksCard x={2}/>
