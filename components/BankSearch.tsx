8606d6aba2071931683b0b5334fa4bce5652638c
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-10-10 22:20:59 +0600

    Search section complete

--- components/BankSearch.tsx
+++ components/BankSearch.tsx
@@ -1,13 +1,24 @@
 'use client';
 import Image from 'next/image';
+import SearchIcon from './Icons/SearchIcon';
+import FilterIcon from './Icons/FilterIcon';
 
 const BankSearch = (): JSX.Element => {
   return (
     <div className='flex-col mx-40 mt-[70px]'>
         <h1 className='text-2xl font-semibold tracking-tight leading-9 text-[#1D2939]'>All Banks</h1>
-        <div className='border-t-2'>
-            <div contentEditable={true} className="border-2 border-[#B3B3B3] h-14 w-[492px] rounded-xl mt-9 text-[#B3B3B3]">
-              Enter your email here...
+        <div className='border-t-2 flex'>
+            <div contentEditable={true} className="flex border-2 border-[#B3B3B3] h-14 w-[492px] py-4 rounded-xl my-9 text-[#B3B3B3]">
+                <SearchIcon/>
+                Search your desire bank
+            </div>
+            <div contentEditable={true} className="flex border-2 border-[#B3B3B3] ml-56 h-14 py-4 px-6 rounded-xl my-9 text-[#B3B3B3]">
+                <FilterIcon/>
+                Filter by district
+            </div>
+            <div contentEditable={true} className="flex border-2 border-[#B3B3B3] ml-9 h-14 py-4 px-6 rounded-xl my-9 text-[#B3B3B3]">
+                <FilterIcon/>
+                Filter by district
             </div>
         </div>
     </div>
