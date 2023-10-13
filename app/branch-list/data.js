04680f0b28a0b4f95a74f85bb1a27757a12a5c9d
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-10-13 18:28:38 +0600

    Add Branch data

---
+++ app/branch-list/data.js
@@ -0,0 +1,13 @@
+export const BranchData = [
+    {
+      Branch_Name: "Aganagar Branch",
+      District: "Dhaka",
+      Division: "Dhaka",
+      Address: "Babul Tower”, B ridge Road, Kadamtali, Aganagar Union , South Keraniganj, Dhaka",
+      Contact: "01787668285,01787668291 08809666 716 250 Ext-121910",
+      Email: "citybank@gmail.com",
+      Swift_Code: "120270136",
+      Routing_No: "120270136"
+    }
+]
+  
---
+++ components/BankTable.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const BankTable = (): JSX.Element => {
+  return (
+    <div className='flex-col w-[1165px]'>
+        <div className="flex">
+
+        </div>
+    </div>
+  );
+};
+
+export default BankTable;
