7e43d1ba4c55e85b83654bd0128127271ecf63d4
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-10-24 23:45:27 +0600

    Update Learn More Section

--- components/Icons/PlayIcon.tsx
+++ components/Icons/PlayIcon.tsx
@@ -3,7 +3,7 @@
 const PlaySvg = (): JSX.Element => {
 
   return (
-    <svg className='my-auto' xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+    <svg className='mr-4 my-auto' xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
       <g filter="url(#filter0_b_98_3008)">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M12.7387 24C19.3661 24 24.7387 18.6274 24.7387 12C24.7387 5.37258 19.3661 0 12.7387 0C6.11129 0 0.738708 5.37258 0.738708 12C0.738708 18.6274 6.11129 24 12.7387 24ZM10.8637 16.4016L17.6137 12.6288C18.1137 12.3493 18.1137 11.6507 17.6137 11.3712L10.8637 7.59836C10.3637 7.31889 9.73871 7.66823 9.73871 8.22717V15.7728C9.73871 16.3318 10.3637 16.6811 10.8637 16.4016Z" fill="#F44E4D"/>
       </g>
--- components/LearnMore.tsx
+++ components/LearnMore.tsx
@@ -1,40 +1,35 @@
 'use client';
 
 import Image from 'next/image';
-import { Button } from 'antd';
 import PlaySvg from './Icons/PlayIcon';
 
 const LearnMore = (): JSX.Element => {
   return (
-    <section className='h-[720px]'>
-        <div className='bg-[#FFEEDD] h-[680px] skew-y-3 my-10'>
-            <div className='mx-40 flex'>
-                <div className='transform -skew-y-3 pt-40'>
-                    <h1 className='text-[42px] font-semibold leading-tight tracking-tight'>
-                        Get even more financial clarity with Nest Finance.
-                    </h1>
-                    <h2 className='text-[22px] mt-6'>
-                    See all your savings, credit cards, and investments together in one place. 
-                    With everything connected, you'll get timely nerdy insights so you can be sure you're making
-                     the right financial decisions for you. Best of all, it's free.
-                    </h2>
-                    <div className='space-x-8 mt-10 flex'>
-                        <Button className='py-6 px-12 h-20 bg-gradient-to-b from-[#7F56D9] to-[#53389e] text-white text-[18px] font-semibold'>Learn More</Button>
-                        <Button className='py-6 px-12 h-20  text-[#F44E4D] text-[18px] border-2 border-[#F44E4D] font-semibold flex space-x-3'>
-                            <PlaySvg/>
-                            Watch Now
-                        </Button>
-                    </div>
-                </div>
-                <div className='mt-36'>
-                    <Image
-                      src="/brand_assets/manIllustr.png"
-                      width={1400}
-                      height={40}
-                      alt="Picture"
-                    />
-                </div>
+    <section className='bg-[#FFEEDD] flex'>
+        <div className='ml-40 mt-16'>
+            <h1 className='text-[28px] font-semibold leading-normal w-[565px] text-[#1D2939]'>
+                Get even more financial clarity with Nest Finance.
+            </h1>
+            <h2 className='mt-6 w-[662px]'>
+            See all your savings, credit cards, and investments together in one place. 
+            With everything connected, you'll get timely nerdy insights so you can be sure you're making
+             the right financial decisions for you. Best of all, it's free.
+            </h2>
+            <div className='space-x-8 mt-12 flex'>
+                <button className='py-4 px-8 rounded-md bg-gradient-to-b from-[#7F56D9] to-[#53389e] text-white text-[18px] font-semibold'>Learn More</button>
+                <button className='py-4 px-8 rounded-md  text-[#F44E4D] text-[18px] border-2 border-[#F44E4D] font-semibold flex space-x-3'>
+                    <PlaySvg/>
+                    Watch Now
+                </button>
             </div>
+        </div>
+        <div className='my-[50px] ml-auto mr-40'>
+            <Image
+              src="/brand_assets/manIllustr.png"
+              width={300}
+              height={40}
+              alt="Picture"
+            />
         </div>
     </section>
   );
