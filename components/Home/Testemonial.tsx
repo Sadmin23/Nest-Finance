87afac6c2c88bf12ff79dfabfc99dc013a5e5a18
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2024-01-27 19:25:19 +0600

    make testemonial layout

--- components/Home/Testemonial.tsx
+++ components/Home/Testemonial.tsx
@@ -9,21 +9,15 @@ import RightArrow from '../Icons/RightArrow';
 
 const Testemonial = (): JSX.Element => {
   return (
-    <section className='bg-[#53389E]'>
-        <div className='mx-40 pt-[59px]'>
-            <h1 className='text-[28px] font-semibold text-white'>
+    <section className='bg-[#FED] pb-10'>
+        <div className='mx-40'>
+            <h1 className='text-[28px] pt-12 font-semibold text-[#1A1A1A]'>
                 Trusted Voices of Finance
             </h1>
-            <h2 className='w-[565px] my-6 text-white font-medium'>
+            <h2 className='w-1/2 mt-4 mb-9 text-[#1A1A1A] font-medium'>
                 Real Stories from Our Satisfied Users: Discover How Our Solutions Are Transforming Financial Experiences.
             </h2>
-            <div className='flex'>
-                <div className='space-x-12 flex mb-14'>
-                    <TestemonialCard/>
-                    <TestemonialCard/>
-                    <TestemonialCard/>
-                </div>
-            </div>
+            <TestemonialCard/>
         </div>
     </section>
   );
--- components/Home/TestemonialCard.tsx
+++ components/Home/TestemonialCard.tsx
@@ -1,39 +1,18 @@
 'use client';
 
 import Image from 'next/image';
-import { Button } from 'antd';
-import Stars from '../Icons/Stars';
 
 const TestemonialCard = (): JSX.Element => {
   return (
-    <div className='w-[365px] h-[211px]  bg-white bg-opacity-50 rounded-md text-[#121111]'>
-      <div className='flex'>
-        <Image
-          src="/brand_assets/person2.png"
-          width={168}
-          height={40}
-          alt="Picture"
-          className='m-3 h-[130px] rounded-2xl'
-        />
-        <div>
-          <h1 className='mt-8 text-xl font-semibold text-[#121111]'>Anita R.</h1>
-          <h2 className='mt-2 mb-1 text-sm font-medium text-[#121111]'>Small Business Owner</h2>
-          <Stars/>
-        </div>
+    <div className='mx-auto w-9/12 h-96 bg-[#ffe3c7] rounded-2xl border-[#ffc991] border-2 text-[#1A1A1A] flex items-center'>
+      <Image src='/brand_assets/test1.png' width={230} height={100} alt='testemonial' className='rounded-2xl ml-4'/>
+      <div className='bg-[#FFD8B1] w-4/6 h-[340px] ml-auto mr-4 rounded-2xl p-6 border-2 border-[#FFC284]'>
+        <h1 className='text-[#1A1A1A]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It has survived not only five centuries, but also the leap into electronic typesetting.</h1>
+        <h1 className='mt-auto text-[#121111] text-2xl font-semibold leading-10'>Anita R.</h1>
+        <h2 className='text-[#676767] font-medium leading-7'>Small Business Owner</h2>
       </div>
-      <h2 className='leading-6 ml-3 text-[#121111]'>The analytics and insights offered by Nest Finance are unparalleled.</h2>
     </div>
   );
 };
 
 export default TestemonialCard;
-
-
-/*
-      <div className='flex-col'>
-        <h2 className='mt-14 text-base font-medium leading-6'>The analytics and insights offered by Nest Finance are unparalleled.</h2>
-        <h1 className='mt-7 text-xl font-semibold leading-normal'>Anita R.</h1>
-        <h2 className='my-2 text-base font-medium leading-normal'>Small Business Owner</h2>
-        <Stars/>
-      </div>
-*/
